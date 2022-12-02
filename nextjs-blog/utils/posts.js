916924d523a4920141400d8d25861f6e401a556d
Adrian Muino <adrian.muino@kaseya.com>
2022-12-02 21:08:50 +0000

    first commit

---
+++ nextjs-blog/utils/posts.js
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, '');
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Instead of the file system, you could
+    // fetch post data from an external API endpoint
+    // const res = await fetch('..');
+    // return res.json();
+
+    // or fetch post data from a database
+    // import someDatabaseSDK from 'someDatabaseSDK'
+    // const databaseClient = someDatabaseSDK.createClient(...)
+    // return databaseClient.query('SELECT posts...')
+
+    // Combine the data with the id
+    return {
+      id,
+      ...matterResult.data,
+    };
+  });
+  // Sort posts by date
+  return allPostsData.sort(({ date: a }, { date: b }) => {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
