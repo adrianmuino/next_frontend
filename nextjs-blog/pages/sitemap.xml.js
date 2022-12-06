f5fe694cf649a6045a47bb88c99e3ec08c507628
Adrian Muino <adrian.muino@kaseya.com>
2022-12-06 21:42:34 +0000

    seo: Add dynamic sitemap.xml generator

---
+++ nextjs-blog/pages/sitemap.xml.js
@@ -0,0 +1,56 @@
+/*
+    Sample sitemap.xml generator for dynamic websites.
+
+    You could do a simple, static sitemap.xml file under /public
+    if your website is a static website.
+*/
+
+const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://jsonplaceholder.typicode.com</loc>
+     </url>
+     <url>
+       <loc>https://jsonplaceholder.typicode.com/guide</loc>
+     </url>
+     ${posts
+       .map(({ id }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+// Needed as the page's main component
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const posts = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
